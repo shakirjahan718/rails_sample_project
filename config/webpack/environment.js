bdb354117f24c71bfd58608d567155ab8db2458a
shakirjahan718 <shakirjahan718@gmail.com>
2021-03-11 01:24:23 +0500

    Yarn add bootstrap jquery popper.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack');
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    Cookies: 'js-cookie'
+  })
+);
+
 module.exports = environment
